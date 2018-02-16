a8284dcc6e0ffae3b51acce2395cba3be2cffeb4
Pianiste <lebras.amandine@yahoo.fr>
2018-02-16 16:34:11 +0100

    add constantes.py file

---
+++ constantes.py
@@ -0,0 +1,34 @@
+"""
+Constantes for Mac Gyver Labyrinth Game
+Amandine Le Bras
+"""
+
+
+#Parameters for windon
+NUMBER_SPITE_COT = 15
+SIZE_SPRITE = 30
+WINDOW_SIDE = NUMBER_SPITE_COT * SIZE_SPRITE
+
+#Customization for the window 
+TITLE_WINDOW = "Mac Gyver Labyrinth"
+PICTURE_ICON = "pictures/Mac_Gyver.png"
+
+#List of game's pictures
+PICTURE_HOME = "pictures/home_mac_gyver.png"
+PICTURE_BACKGROUND = "pictures/background.jpg"
+PICTURE_WALL = "pictures/wall.png"
+PICTURE_START = "pictures/start.png"
+PICTURE_ETHER = "pictures/ether.png"
+PICTURE_NEEDLE = "pictures/needle.png"
+PICTURE_PLASTIC_TUBE = "pictures/plastic_tube.png"
+PICTURE_MAC_GYVER = "pictures/Mac_Gyver.png"
+PICTURE_MURDOC = "pictures/Murdoc.png"
+PICTURE_DEAD = "pictures/loose.png"
+PICTURE_WIN = "pictures/macgyver_win.png"
+
+#List of game's music
+MUSIC_HOME = "music/8_bit_macgyver.ogg"
+MUSIC_WIN = "music/son_win.ogg"
+MUSIC_DEAD = "music/dead.ogg"
+
+
